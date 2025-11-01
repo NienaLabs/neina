6c7f3bd81a77cb899ad72c31a84da3b8858acf62
Your Name <swiftwilliams868@gmail.com>
2025-11-01 03:59:58 +0000

    feat: set-up authentication
    -added authentication with better-auth
    -added sign-up,sign-in,account management and google social login using
    better-auth-ui to speed things up

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,11 +1,45 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 // If your Prisma file is located elsewhere, you can change the path
-import { PrismaClient } from "@/generated/prisma/client";
+import { PrismaClient } from "@/lib/generated/prisma/client";
+import { EmailTemplate } from "@daveyplate/better-auth-ui/server";
 
 const prisma = new PrismaClient();
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
         provider: "postgresql", // or "mysql", "postgresql", ...etc
     }),
+      emailAndPassword: { 
+    enabled: true, 
+  }, 
+  socialProviders: { 
+    google: { 
+      clientId: process.env.GOOGLE_CLIENT_ID as string, 
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string, 
+    }, 
+  }, 
+   emailVerification: {
+        sendVerificationEmail: async ({ user, url, token }, request) => {
+            const name = user.name || user.email.split("@")[0]
+            await fetch("https://api.brevo.com/v3/smtp/email", {
+                  method: "POST",
+                 headers: {
+                 "accept": "application/json",
+                 "content-type": "application/json",
+                 "api-key": process.env.BREVO_API_KEY!,
+             },
+            body: JSON.stringify({
+           sender: { name: "John Doe", email: "john@mycompany.com" },
+             to: [{ email: "recipient@example.com", name: "Jane" }],
+             subject: `Hello ${name}`,
+             htmlContent: `<a href={"${url}"}>Click this link to verify your account </a>`,
+  }),
+})
+  .then((res) => res.json())
+  .then((data) => console.log("Email sent:", data))
+  .catch((err) => console.error("Error sending email:", err));
+
+        },
+        autoSignInAfterVerification: true,
+        sendOnSignUp: true}
 });

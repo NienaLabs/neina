f3c81317362c80999389e2d51ae0e352a1179cdd
Yaw Adomako <adomakoy42@gmail.com>
2025-11-09 13:29:41 +0000

    New Design on the Landing Page

---
+++ app/components/landing/Hero.tsx
@@ -0,0 +1,10 @@
+import { HeroGeometric } from "@/components/ui/ElegantShape"
+
+
+function Hero() {
+    return <HeroGeometric badge="Evan And Williams"
+            title1 = "Land Your Dream Job"
+            title2 = "With AI-Powered Tools" />
+}
+
+export { Hero}

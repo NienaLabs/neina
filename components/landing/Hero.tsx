3eb83382f342428c307c44f3bef76c65778e38e9
Your Name <swiftwilliams868@gmail.com>
2025-11-29 13:32:13 +0000

    Fix: Change badge

--- components/landing/Hero.tsx
+++ components/landing/Hero.tsx
@@ -2,7 +2,7 @@ import { HeroGeometric } from "@/components/ui/ElegantShape"
 
 
 function Hero() {
-    return <HeroGeometric badge="Evan And Williams"
+    return <HeroGeometric badge="Lets build your dream together"
             title1 = "Achieve Your Career Goals"
             title2 = "With AI-Powered Tools" />
 }

651c6c3d51c2b94c90e912435c703fa21097d877
Your Name <swiftwilliams868@gmail.com>
2025-11-29 03:15:23 +0000

    Fix: Authentication UI

--- app/auth/[path]/page.tsx
+++ app/auth/[path]/page.tsx
@@ -11,8 +11,8 @@ export default async function AuthPage({ params }: { params: Promise<{ path: str
     const { path } = await params
     
     return (
-        <main className="container flex grow flex-col items-center justify-center self-center p-4 md:p-6">
-            <AuthView path={path} />
+        <main className="flex grow jigsaw-background min-h-screen flex-col items-center justify-center  p-4 md:p-6">
+            <AuthView path={path} className="bg-indigo-100 border-black/30 "/>
         </main>
     )
 }
